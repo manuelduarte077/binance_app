61968c2f66c9cd556b63c68bd4f4446bff6bfded
Don Manuel <carlosd99m@gmail.com>
2024-04-24 15:25:06 -0600

    wip: initial code

---
+++ README.md
@@ -0,0 +1,3 @@
+# binance_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/wondertech/binance_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.wondertech.binance_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
